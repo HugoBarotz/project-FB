ff2a3336c0027bcc248b60575a1084b07b4c8e85
Hugo Le Port <hugo.leport@gmail.com>
2023-07-24 16:15:18 +0200

    Project Firebase first commit

---
+++ src/main/java/com/hugo/project/firebase/FirebaseInitialization.java
@@ -0,0 +1,2 @@
+package com.hugo.project.firebase;public class FirebaseInitialization {
+}
--- src/main/java/com/hugo/project/repository/ProjectRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hugo.project.repository;
-
-import com.hugo.project.model.Project;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepo extends ElasticsearchRepository<Project, String> {
-}
